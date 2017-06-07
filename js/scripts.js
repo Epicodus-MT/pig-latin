91448311aa64590bbbc821e69d20b877bf812d66
Mick Sexton and Mara Timberlake <mara@>
2017-06-07 11:56:18 -0700

    clean up indentation

--- js/scripts.js
+++ js/scripts.js
@@ -13,32 +13,47 @@ $(document).ready(function() {
     var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
     for (var i = 0; i < wordsArray.length; i++) {
       console.log('1st for loop');
-      for (var j = 0; j < vowels.length; j++) {
-        console.log('2nd for loop');
-        if(wordsArray[i].charAt(0) === vowels[j]) {
-          wordsArray[i] = wordsArray[i].concat("way");
-          console.log(wordsArray[i]);
-          j = vowels.length;
-        } else if (wordsArray[i].charAt(0) === 'Q' || wordsArray[i].charAt(0) === 'q') {
-            if(wordsArray[i].charAt(1) === 'U' || wordsArray[i].charAt(1) === 'u') {
-              console.log("Qu detected");
-              var temp = wordsArray[i].charAt(0) + wordsArray[i].charAt(1);
-              wordsArray[i] = wordsArray[i].slice(2);
-              wordsArray[i] = wordsArray[i].concat(temp);
-              console.log(wordsArray[i]);
+      var lettersArray = [];
+      lettersArray = wordsArray[i].split('');
 
-              //var temp = data;
-              //var copy = data.slice();
-              // Add some values to data, ref will change but copy won't
-              //data.push(6, 7, 8);
+      //for (var k = 0; k < lettersArray.length; k++) {
+        //var tempCons = "";
 
-            }
+        for (var j = 0; j < vowels.length; j++) {
+          console.log('2nd for loop');
+          if(wordsArray[i].charAt(0) === vowels[j]) {
+            wordsArray[i] = wordsArray[i].concat("way");
+            console.log(wordsArray[i]);
             j = vowels.length;
+            k = lettersArray.length;
+          } else if (wordsArray[i].charAt(0) === 'Q' || wordsArray[i].charAt(0) === 'q') {
+              if(wordsArray[i].charAt(1) === 'U' || wordsArray[i].charAt(1) === 'u') {
+                console.log("Qu detected");
+                var temp = wordsArray[i].charAt(0) + wordsArray[i].charAt(1);
+                wordsArray[i] = wordsArray[i].slice(2);
+                wordsArray[i] = wordsArray[i].concat(temp + "ay");
+                console.log(wordsArray[i]);
+
+              }
+              j = vowels.length;
+              k = lettersArray.length;
+
+          } else {
+              var tempCons = "";
+              for (var k = 0; k < lettersArray.length; k++) {
+                console.log(lettersArray[k]);
+              }
+              k = lettersArray.length;
+
+          //   tempCons = tempCons.concat(lettersArray[k]);
+          //   console.log = tempCons;
+
+          }
+
 
         }
 
-
       }
-    }
+    //}
   });
 });
